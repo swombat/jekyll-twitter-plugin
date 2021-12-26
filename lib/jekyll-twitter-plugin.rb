7fb7dcf8bdfe67867207be668cebfef0324cbee3
Daniel Tenner <danieltenner@daniels-macbook-pro.home>
2021-12-26 21:24:18 +0100

    ..

--- lib/jekyll-twitter-plugin.rb
+++ lib/jekyll-twitter-plugin.rb
@@ -202,7 +202,11 @@ module TwitterJekyll
     def html_output_for(response)
       body = (response.html if response) || ERROR_BODY_TEXT
 
+      puts "===\nBody before: #{body}"
+
       body = body.gsub('<script async="" src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>', "")
+                        
+      puts "===\nBody after: #{body}"
 
       "<div class='jekyll-twitter-plugin'>#{body}</div>"
     end
